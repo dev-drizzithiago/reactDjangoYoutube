f46b8becf270a2fcb1ce0551c791f30f5637060e
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-11 13:19:10 -0300

    Update EventsBtns.js

--- React/youtube/src/componentes/EventsBtns.js
+++ React/youtube/src/componentes/EventsBtns.js
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import LinkBancoDados from "./LinkBancoDados";
 
 const EventsBtns = () => {
+    const [mostrarLink, setMostrarLink] = useState(false)
 
     const eventoBtn = () => {
-        alert('Clicou');
+        setMostrarLink(true)
     }
 
     return (
         <div>
-            <button onClick={eventoBtn}>aplicar</button>
+            <button onClick={eventoBtn}>aplicar</button>     
+            {mostrarLink && <LinkBancoDados/>}       
         </div>
-    );
+    );    
 };
 
 export default EventsBtns;
