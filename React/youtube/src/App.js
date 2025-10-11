6534de9c6c77977b9f99efa17f710759bbc9cc8f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-11 13:07:45 -0300

    Update App.js

--- React/youtube/src/App.js
+++ React/youtube/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+
 import FormularioLinkYoutube from './componentes/FormularioLinkYoutube';
+import LinkBancoDados from './componentes/LinkBancoDados';
+import EventsBtns from './componentes/EventsBtns'
 
 function App() {
   return (
     <div className="App">
       <h1> Ola, mundo!</h1>
       <FormularioLinkYoutube />
+      <LinkBancoDados />
+      <EventsBtns />
     </div>
   );
 }
