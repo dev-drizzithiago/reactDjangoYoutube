57786d120c393781599ce1087d51f9f6f8fd9131
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-11 13:07:53 -0300

    Create LinkBancoDados.js

---
+++ React/youtube/src/componentes/LinkBancoDados.js
@@ -0,0 +1,15 @@
+// Para colocar um qualquer elemento de html, é preciso esta sempre dentro de uma tag<div> => exemplo
+const LinkBancoDados = () => {
+
+    const meuNome = 'Thiago Pinheiro'
+
+    return (
+        <div>
+            <h2>
+                {meuNome}
+            </h2>
+        </div>
+    );
+};
+
+export default LinkBancoDados
