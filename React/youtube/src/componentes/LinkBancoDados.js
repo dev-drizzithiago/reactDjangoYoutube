dbac4cd61b2eccd44dd2ffa3908fec220e31d022
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-12 16:49:32 -0300

    Update LinkBancoDados.js

--- React/youtube/src/componentes/LinkBancoDados.js
+++ React/youtube/src/componentes/LinkBancoDados.js
@@ -1,15 +1,39 @@
 // Para colocar um qualquer elemento de html, é preciso esta sempre dentro de uma tag<div> => exemplo
+import { useState } from "react";
+
 const LinkBancoDados = () => {
 
-    const meuNome = 'Thiago Pinheiro'
+    const [meuNome] = useState([
+        {
+        'link_tube': 'https://www.google.com',
+        'autor_link': 'Phil Collins - Topic',
+        'titulo_link': 'Another Day in Paradise (2016 Remaster)',
+        'duracao': '322',
+        'miniatura': 'https://i.ytimg.com/vi/qkDVozHVeM8/sddefault.jpg'
+        }
+    ])
 
     return (
         <div>
-            <h2>
-                {meuNome}
-            </h2>
+            {meuNome.map((item) => (
+                <div><p>
+                    <a href={item.link_tube} target="_blank" rel="noopener noreferrer">
+                        <img src="/img/imgBtns/download.png" alt="download" className="imgBtn imgBtnDownload" />
+                    </a>                    
+                    <a href={item.link_tube} target="_blank" rel="noopener noreferrer">
+                        <img src="/img/imgBtns/remover.png" alt="remover" className="imgBtn imgBtnRemover" />
+                    </a>
+                    <a href={item.link_tube} target="_blank" rel="noopener noreferrer">
+                        <img src="/img/imgBtns/youtube.png" alt="link" className="imgBtn imgBtnLink" />
+                    </a>
+                </p>
+                <h6>{item.autor_link} - {item.titulo_link}</h6>
+                </div>
+                               
+            ))}
         </div>
     );
 };
 
 export default LinkBancoDados
+
