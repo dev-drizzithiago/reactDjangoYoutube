3d5ef6d777515029914bf9bea285e8784d032c31
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-12 16:49:26 -0300

    Update FormularioLinkYoutube.js

--- React/youtube/src/componentes/FormularioLinkYoutube.js
+++ React/youtube/src/componentes/FormularioLinkYoutube.js
@@ -1,18 +1,20 @@
-import btnImgAdicionar from "../assets/img/imgBtns/adicionar.png";
 
 const FormularioLinkYoutube = () => {
+
+  const btnAddLinkyoutube = () => {
+    alert('Teste')
+  }
+
   return (
     <div>
       <h2>Formulario para Link do Youtube</h2>
-      <form className="formsLinkYoutube" method="POST">
-        <div>
+      
+        <div className="divLabelInput">
           <label className="label labelLink" htmlFor="link">Cole o Link do Video:</label>
-          <input type="text" className="inputLinkYoutube" name="link" />
+          <input type="text" className="inputText inputLinkYoutube" name="link" />
         </div>
-        <button className="buttonSubmit" type="submit">
-            <img src={btnImgAdicionar} alt="adicionar"/>
-        </button>
-      </form>
+        <img src="/img/imgBtns/adicionar.png" alt="adicionar" className="imgBtn" onClick={btnAddLinkyoutube}/>
+             
     </div>
   );
 };
