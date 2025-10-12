e411a60ff79ffc4e076516153970c7cd600e1da2
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-12 16:49:12 -0300

    Update App.js

--- React/youtube/src/App.js
+++ React/youtube/src/App.js
@@ -1,18 +1,35 @@
 import './App.css';
 
+import LogoYoutube from './componentes/LogoYoutube'
 import FormularioLinkYoutube from './componentes/FormularioLinkYoutube';
 import LinkBancoDados from './componentes/LinkBancoDados';
 import EventsBtns from './componentes/EventsBtns'
+import PlayerMidiasMp3 from './componentes/PlayerMidiasMp3'
 
 function App() {
   return (
     <div className="App">
-      <h1> Ola, mundo!</h1>
-      <FormularioLinkYoutube />
-      <LinkBancoDados />
-      <EventsBtns />
+      <div className='logoPrincipal'>
+        <LogoYoutube />
+      </div>
+      <div className='divPgPrincipal formsInputLink'>
+        <FormularioLinkYoutube />
+      </div>
+      <div className='containePrincipal'>
+        <div className='divPgPrincipal viewLinksyoutube'>
+          <LinkBancoDados/>
+        </div>
+
+        <div className='divPgPrincipal viewplayerMp3'>
+          <PlayerMidiasMp3/>
+        </div>
+
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+/** RAFCE Para criar uma estrutura no componentes */
