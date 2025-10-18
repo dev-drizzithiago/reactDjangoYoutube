50c9cd9aeddd5797f069a9dad0098125c77ab201
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-18 08:22:53 -0300

    Update views.py

--- Django/coreYoutube/views.py
+++ Django/coreYoutube/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+import json
+
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
 def requestBaseDados(request):
+    if request.method == 'POST':
+        dados_json = json.loads(request.body)
 
-
-    return JsonResponse({
-        'mensagem': 'Teste Django'
-    })
+        return JsonResponse({
+            'mensagem': 'Teste Django'
+        })
+    else:
+        return JsonResponse({
+            'mensagem': 'O method precisa ser POST',
+        })
 
