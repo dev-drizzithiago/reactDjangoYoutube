980e92f8d68bf0f609dc3e5f8128ee2698a6875d
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-18 11:30:41 -0300

    Update requestDjango.js

--- React/youtube/src/componentes/requestDjango.js
+++ React/youtube/src/componentes/requestDjango.js
@@ -1,25 +1,34 @@
 import { useState, useEffect } from "react"
 
-const requestDjango = () => {  
+const requestDjango = (urlDjango, payload) => {  
     const [dado, setDados] = useState([])
     const [carregando, setCarregando] = useState(false)
+    const urlDjango = null
 
-    return (
-        <div className="divPrincipalRequestDjango">
-            useEffect(() => {
-                fetch(urlDjango, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-CSRFToken': getCookie('csrftoken'),
-                    },
-                    credentials: 'include',
-                    body: JSON.stringify({action: 'requesting'}),
-                });
-            }, [urlDjango]);
-            if (carregando) <p>Carregando...</p>
-        </div>
-    )
+    useEffect(() => {                
+        fetch(urlDjango, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCookie('csrftoken'),
+            },
+            credentials: 'include',
+            body: JSON.stringify(payload),
+        })
+        .then(response => response.json())
+        .then(data => {
+            setDados(data);
+            setCarregando(false);
+        })
+        .catch (error => {
+            console.error('Erro na requisição: ', error)
+            setCarregando(false)
+        })
+    }, [urlDjango, JSON.stringify(payload)]);
+
+    if (carregando) <p>Carregando...</p>
+
+    return {dado, carregando}
 }
 
 export default requestDjango
