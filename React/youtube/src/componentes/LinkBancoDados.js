14647b917fc471c5f895129780c7375ffbd8c3e4
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-18 12:59:26 -0300

    Update LinkBancoDados.js

--- React/youtube/src/componentes/LinkBancoDados.js
+++ React/youtube/src/componentes/LinkBancoDados.js
@@ -2,31 +2,32 @@
 import useRequestDjango from "./useRequestDjango";
 
 const LinkBancoDados = () => {
-
-    const {dados, carregando} = useRequestDjango("http://localhost:8000/requestBaseDados/", {action: 'listar'})
-
+    
+    const {dados, carregando} = useRequestDjango("http://localhost:3000/requestBaseDados/", {action: 'listar'})
+   
     if (carregando) return <p>Carregando...</p>;
 
     return (
-        <div>            
+        <div>
+            <h1>Teste</h1>            
             <div className="divLinkYoutube">
-            {dados.map((item) => (
-                <div>
-                    <p>
-                        <a href={item.link_tube} target="_blank" rel="noopener noreferrer">
-                            <img src="/img/imgBtns/download.png" alt="download" className="imgBtn imgBtnDownload" />
-                        </a>                    
-                        <a href={item.link_tube} target="_blank" rel="noopener noreferrer">
-                            <img src="/img/imgBtns/remover.png" alt="remover" className="imgBtn imgBtnRemover" />
-                        </a>
-                        <a href={item.link_tube} target="_blank" rel="noopener noreferrer">
-                            <img src="/img/imgBtns/youtube.png" alt="link" className="imgBtn imgBtnLink" />
-                        </a>
-                    </p>
-                <h6>{item.autor_link} - {item.titulo_link}</h6>
-                </div>                                
-            ))}
-            </div>
+                {dados.map((item) => (
+                    <div>
+                        <p>
+                            <a href={item.link_tube} target="_blank" rel="noopener noreferrer">
+                                <img src="/img/imgBtns/download.png" alt="download" className="imgBtn imgBtnDownload" />
+                            </a>                    
+                            <a href={item.link_tube} target="_blank" rel="noopener noreferrer">
+                                <img src="/img/imgBtns/remover.png" alt="remover" className="imgBtn imgBtnRemover" />
+                            </a>
+                            <a href={item.link_tube} target="_blank" rel="noopener noreferrer">
+                                <img src="/img/imgBtns/youtube.png" alt="link" className="imgBtn imgBtnLink" />
+                            </a>
+                        </p>
+                    <h6>{item.autor_link} - {item.titulo_link}</h6>
+                    </div>                                
+                ))}
+            </div>            
         </div>
     );
 };
