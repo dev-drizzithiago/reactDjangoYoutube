9efa04926023e234a1185edd920e426051475ce1
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-18 08:14:41 -0300

    Create urls.py

---
+++ Django/coreYoutube/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import index, requestBaseDados
+
+urlpatterns = [
+    path('index', index, name='index'),
+    path('requestBaseDados', requestBaseDados, name='requestBaseDados')
+]
