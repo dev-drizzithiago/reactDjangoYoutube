bbd2223a42f7176f002869f4177efb81588e7844
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-18 07:54:32 -0300

    Update App.js

--- React/youtube/src/App.js
+++ React/youtube/src/App.js
@@ -3,27 +3,24 @@ import './App.css';
 import LogoYoutube from './componentes/LogoYoutube'
 import FormularioLinkYoutube from './componentes/FormularioLinkYoutube';
 import LinkBancoDados from './componentes/LinkBancoDados';
-import EventsBtns from './componentes/EventsBtns'
-import PlayerMidiasMp3 from './componentes/PlayerMidiasMp3'
+
 
 function App() {
   return (
     <div className="App">
+
       <div className='logoPrincipal'>
         <LogoYoutube />
       </div>
+
       <div className='divPgPrincipal formsInputLink'>
         <FormularioLinkYoutube />
       </div>
+
       <div className='containePrincipal'>
         <div className='divPgPrincipal viewLinksyoutube'>
           <LinkBancoDados/>
         </div>
-
-        <div className='divPgPrincipal viewplayerMp3'>
-          <PlayerMidiasMp3/>
-        </div>
-
       </div>
     </div>
   );
