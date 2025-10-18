9e2129f6054fd1df34bdf68aec5adad60200555d
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-18 08:22:50 -0300

    Update models.py

--- Django/coreYoutube/models.py
+++ Django/coreYoutube/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.db.models import PROTECT
 
-# Create your models here.
+class Base(models.Model):
+    data_criacao = models.DateField('data_criacao', auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class DadosYoutube(Base):
+    id_dados = models.AutoField(primary_key=True)
+    autor_link = models.CharField(max_length=255)
+    titulo_link = models.CharField(max_length=255)
+    duracao = models.IntegerField()
+    miniatura = models.CharField(max_length=255)
+    link_tube = models.CharField(max_length=255)
+
+class MoviesSalvasServidor(Base):
+    id_movies = models.AutoField(primary_key=True)
+    nome_arquivo = models.CharField(max_length=255, null=True)
+    path_arquivo = models.CharField(max_length=255, null=True)
+    duracao_midia = models.IntegerField()
+    path_miniatura = models.FileField(upload_to='miniaturas/', max_length=255)
+    dados_youtube = models.ForeignKey(DadosYoutube, on_delete=PROTECT)
+
+class MusicsSalvasServidor(Base):
+    id_music = models.AutoField(primary_key=True)
+    nome_arquivo = models.CharField(max_length=255, null=True)
+    path_arquivo = models.CharField(max_length=255, null=True)
+    duracao_midia = models.IntegerField()
+    path_miniatura = models.FileField(upload_to='miniaturas/', max_length=255)
+    dados_youtube = models.ForeignKey(DadosYoutube, on_delete=PROTECT)
