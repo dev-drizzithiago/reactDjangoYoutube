487904b135788f75b67901bfb61c76ee0d4bab6e
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-19 15:40:05 -0300

    Update FormularioLinkYoutube.js

--- React/youtube/src/componentes/FormularioLinkYoutube.js
+++ React/youtube/src/componentes/FormularioLinkYoutube.js
@@ -1,21 +1,22 @@
-
+import { useRef } from "react";
+import useRequestDjango from "./useRequestDjango";
 
 const FormularioLinkYoutube = () => {
-
-  const btnAddLinkyoutube = () => {
-    alert('Teste')
+  const refLink = useRef();
+  const useDefGravandoLink = () => {
+    const linkYoutube = refLink.current.value    
+    const {dados, carregando} = useRequestDjango("http://localhost:8000/requestAddLinks/", {'link': linkYoutube})
+    if (carregando) {<h1>carregando...</h1>}
   }
 
   return (
-    <div>
-      <h2>Formulario para Link do Youtube</h2>
-      
+    <div>      
+      <h2>Formulario para Link do Youtube</h2>      
         <div className="divLabelInput">
           <label className="label labelLink" htmlFor="link">Cole o Link do Video:</label>
-          <input type="text" className="inputText inputLinkYoutube" name="link" />
+          <input type="text" className="inputText inputLinkYoutube" name="link" ref={refLink}/>
         </div>
-        <img src="/img/imgBtns/adicionar.png" alt="adicionar" className="imgBtn" onClick={btnAddLinkyoutube}/>
-             
+        <img src="/img/imgBtns/adicionar.png" alt="adicionar" className="imgBtn" onClick={useDefGravandoLink}/>
     </div>
   );
 };
