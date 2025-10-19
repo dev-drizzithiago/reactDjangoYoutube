77379dc204a7e296d313b0d7bc8197a138b1957c
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-19 15:40:09 -0300

    Update LinkBancoDados.js

--- React/youtube/src/componentes/LinkBancoDados.js
+++ React/youtube/src/componentes/LinkBancoDados.js
@@ -3,28 +3,29 @@ import useRequestDjango from "./useRequestDjango";
 
 const LinkBancoDados = () => {
     
-    const {dados, carregando} = useRequestDjango("http://localhost:8000/requestBaseDados/", {action: 'listar'})
-   
+    const {dados, carregando} = useRequestDjango("http://localhost:8000/requestBaseDados/", {'action': 'listar'})
+    console.log(dados)
+
     if (carregando) return <p>Carregando...</p>;
 
     return (
         <div>
-            <h1>Teste</h1>            
-            <div className="divLinkYoutube">
+            <h1>teste</h1>   
+            <div className="divLinkYoutube">              
                 {dados.map((item) => (
                     <div>
                         <p>
-                            <a href={item.link_tube} target="_blank" rel="noopener noreferrer">
+                            <a href="/" target="_blank" rel="noopener noreferrer">
                                 <img src="/img/imgBtns/download.png" alt="download" className="imgBtn imgBtnDownload" />
                             </a>                    
-                            <a href={item.link_tube} target="_blank" rel="noopener noreferrer">
+                            <a href="/" target="_blank" rel="noopener noreferrer">
                                 <img src="/img/imgBtns/remover.png" alt="remover" className="imgBtn imgBtnRemover" />
                             </a>
-                            <a href={item.link_tube} target="_blank" rel="noopener noreferrer">
+                            <a href="/" target="_blank" rel="noopener noreferrer">
                                 <img src="/img/imgBtns/youtube.png" alt="link" className="imgBtn imgBtnLink" />
                             </a>
                         </p>
-                    <h6>{item.autor_link} - {item.titulo_link}</h6>
+                        <h6>{item.autor_link} - {item.titulo_link}</h6>
                     </div>                                
                 ))}
             </div>            
