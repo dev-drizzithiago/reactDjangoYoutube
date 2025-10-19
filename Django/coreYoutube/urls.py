30055a236e146ff90ca8090599e81aed786470fc
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-19 15:36:20 -0300

    Update urls.py

--- Django/coreYoutube/urls.py
+++ Django/coreYoutube/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, requestBaseDados
+from .views import index, requestBaseDados, requestAddLinks
 
 urlpatterns = [
     path('index', index, name='index'),
-    path('requestBaseDados/', requestBaseDados, name='requestBaseDados')
+    path('requestBaseDados/', requestBaseDados, name='requestBaseDados'),
+    path('requestAddLinks/', requestAddLinks, name='requestAddLinks'),
 ]
