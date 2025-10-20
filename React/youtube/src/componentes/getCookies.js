dd2b75bb059508ba12171faa586ac22fd117cdf0
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-20 12:17:38 -0300

    Update getCookies.js

--- React/youtube/src/componentes/getCookies.js
+++ React/youtube/src/componentes/getCookies.js
@@ -1,20 +1,20 @@
 
+function getCookies(name) {
 
-const getCookies = () => {
-    function getCookie(name) {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            const cookies = document.cookie.split(';');
-            for (let i=0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break
-                }
+    let cookieValue = null;
+
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i=0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break
             }
         }
-        return cookieValue;
     }
+
+    return cookieValue;
 }
 
 export default getCookies;
