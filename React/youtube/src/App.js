5e47154db2b437ca3c5ab2804e57eba1849d29b8
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-20 14:19:38 -0300

    Update App.js

--- React/youtube/src/App.js
+++ React/youtube/src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import LogoYoutube from './componentes/LogoYoutube'
 import FormularioLinkYoutube from './componentes/FormularioLinkYoutube';
 import LinkBancoDados from './componentes/LinkBancoDados';
-
+import useRequesicaoTesteApi from './componentes/useRequesicaoTesteApi';
 
 function App() {
   return (
     <div className="App">
+
+
+      <useRequesicaoTesteApi />
+
+      
       <div className='logoPrincipal'>
         <LogoYoutube />
+        
       </div>
 
       <div className='divPgPrincipal formsInputLink'>
