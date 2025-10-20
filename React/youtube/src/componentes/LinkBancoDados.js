46612dcdc944ed4fa06f4be5788567844e0c2486
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-20 18:47:21 -0300

    Update LinkBancoDados.js

--- React/youtube/src/componentes/LinkBancoDados.js
+++ React/youtube/src/componentes/LinkBancoDados.js
@@ -9,25 +9,27 @@ const LinkBancoDados = () => {
 
     return (
         <div>
-            <h1>teste</h1>   
-            <div className="divLinkYoutube">              
+            <h1> Links para download </h1>
+            
+            <div className="content">
                 {dados.map((item, index) => (
-                    <div>
-                        <p>
-                            <a href="/" target="_blank" rel="noopener noreferrer">
-                                <img src="/img/imgBtns/download.png" alt="download" className="imgBtn imgBtnDownload" />
-                            </a>                    
-                            <a href="/" target="_blank" rel="noopener noreferrer">
-                                <img src="/img/imgBtns/remover.png" alt="remover" className="imgBtn imgBtnRemover" />
-                            </a>
-                            <a href="/" target="_blank" rel="noopener noreferrer">
-                                <img src="/img/imgBtns/youtube.png" alt="link" className="imgBtn imgBtnLink" />
-                            </a>
-                        </p>
-                        <h6>{item.autor_link} - {item.titulo_link}</h6>
+                    <div className="linksYoutube">
+                    <p>
+                    <a href="/" target="_blank" rel="noopener noreferrer">
+                        <img src="/img/imgBtns/download.png" alt="download" className="imgBtn imgBtnDownload" />
+                    </a>                    
+                    <a href="/" target="_blank" rel="noopener noreferrer">
+                        <img src="/img/imgBtns/remover.png" alt="remover" className="imgBtn imgBtnRemover" />
+                    </a>
+                    <a href="/" target="_blank" rel="noopener noreferrer">
+                        <img src="/img/imgBtns/youtube.png" alt="link" className="imgBtn imgBtnLink" />
+                    </a>
+                    </p>
+                    <h6>{item.autor_link} - {item.titulo_link}</h6>
                     </div>                                
-                ))}
-            </div>            
+                ))}                
+            </div>
+            
         </div>
     );
 };
