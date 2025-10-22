059a765c852587c7c15c7aae4f7fb1236d7631dd
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-21 22:05:57 -0300

    Update FormularioLinkYoutube.js

--- React/youtube/src/componentes/FormularioLinkYoutube.js
+++ React/youtube/src/componentes/FormularioLinkYoutube.js
@@ -1,8 +1,10 @@
 import { useRef, useState } from "react";
 import sendRequestDjango from "./sendRequestDjango";
-
+import "./FormularioLinkYoutube.css"
 const FormularioLinkYoutube = () => {
     const [responseDados, setDados] = useState(null)
+    const [btnLimparForms, setLimparForms] = useState(null)
+
     const refLink = useRef();
 
     const useDefGravandoLink = async () => {
@@ -10,16 +12,27 @@ const FormularioLinkYoutube = () => {
 
       const responseDados = await sendRequestDjango("http://localhost:8000/requestAddLinks/", {'link': linkYoutube})
       setDados(responseDados)
-  }
+    }
+
+    const useDefBtnLimparInput = () => {
+      if (refLink.current) {
+        refLink.value = '';
+        setLimparForms('');
+      }
+    }
 
   return (
-    <div>      
+    <div className="returnFormsLink">
       <h2>Formulario para Link do Youtube</h2>      
         <div className="divLabelInput">
           <label className="label labelLink" htmlFor="link">Cole o Link do Video:</label>
           <input type="text" className="inputText inputLinkYoutube" name="link" ref={refLink}/>
         </div>
-        <img src="/img/imgBtns/adicionar.png" alt="adicionar" className="imgBtn" onClick={useDefGravandoLink}/>
+        <div className="divBtnImgAdd">
+          <img src="/img/imgBtns/adicionar.png" alt="adicionar" className="imgBtn btnAdd" onClick={useDefGravandoLink}/>
+          <img src="/img/imgBtns/limpar.png" alt="adicionar" className="imgBtn btnLimpar" onClick={useDefBtnLimparInput}/>
+        </div>
+        
     </div>
   );
 };
