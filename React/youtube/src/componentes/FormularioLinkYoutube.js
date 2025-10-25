a0f67aba7adc3fa191ca8d88e06085174c07eb1b
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-25 17:51:07 -0300

    Update FormularioLinkYoutube.js

--- React/youtube/src/componentes/FormularioLinkYoutube.js
+++ React/youtube/src/componentes/FormularioLinkYoutube.js
@@ -1,27 +1,39 @@
 import { useRef, useState } from "react";
 import sendRequestDjango from "./sendRequestDjango";
+import LinkBancoDados from "./LinkBancoDados";
 import "./FormularioLinkYoutube.css"
 
 
 const FormularioLinkYoutube = () => {
     const [btnLimparForms, setLimparForms] = useState(null)
     const [responseAlertaDjango, setresponseAlertaDjango] = useState(null)
+    const [carregando, setCarregando] = useState(null)
+    const [atualizarLinks, setAtualizeLinks] = useState(false)
 
     const refLink = useRef();
 
+    /** FUNÇÃO PARA ADICIONAR LINK */
     const useDefGravandoLink = async () => {
-
       const linkYoutube = refLink.current.value
       setLimparForms('responseDados');
+      setCarregando(true)
+
       const responseDados = await sendRequestDjango("http://localhost:8000/requestAddLinks/", {'link': linkYoutube})
+      
       setresponseAlertaDjango(responseDados.mensagem);
       console.log(responseDados)
+      setCarregando(false)
+
+      if (responseDados.erro_processo === 0){
+        setAtualizeLinks(true)
+      }
       
       setTimeout(() => {
           setresponseAlertaDjango('')
         }, 15000);        
     }
-
+    
+    /** FUNÇÃO PARA LIMPAR O CAMPO DE LINK */
     const useDefBtnLimparInput = () => {      
       if (refLink.current) {
         refLink.current.value = '';
@@ -30,15 +42,17 @@ const FormularioLinkYoutube = () => {
       }
     }
 
-  return (
+  return (    
     <div className="returnFormsLink">
+      {atualizarLinks && <LinkBancoDados />}
       <h2>Formulario para Link do Youtube</h2>      
         <div className="divLabelInput">
           <label className="label labelLink" htmlFor="link">Cole o Link do Video:</label>
           <input type="text" className="inputText inputLinkYoutube" name="link" ref={refLink}/>
         </div>
 
-        <div>
+        <div>                   
+          {carregando && <img src="/img/imgBtns/loading.gif" alt="Carregando..."/>}
           <h3 className="mensagemAlerta">{responseAlertaDjango}</h3>
         </div>
         
