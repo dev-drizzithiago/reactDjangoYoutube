b21c69ef0b0693d2cee18f79cc8055cd6c89ab11
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-26 20:20:07 -0300

    Update useRequestDjango.js

--- React/youtube/src/componentes/useRequestDjango.js
+++ React/youtube/src/componentes/useRequestDjango.js
@@ -1,34 +1,25 @@
-import { useState, useEffect } from "react"
 import getCookies from "./getCookies"
 
 const useRequestDjango = (urlDjango, payload) => {
     const payloadString = JSON.stringify(payload)
-
-    const [dados, setDados] = useState([])
-    const [carregando, setCarregando] = useState(true)
-
-    useEffect(() => {
-        fetch(urlDjango, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken':  getCookies('csrftoken'),
-            },
-            body: payloadString,
-            credentials: 'include',
-        })
-        .then(response => response.json())
-        .then(data => {
-            setDados(data.dados_django);
-            setCarregando(false);
-        })
-        .catch (error => {
-            console.error('Erro na requisição: ', error)
-            setCarregando(false)
-        })
-    }, [urlDjango, payloadString]);
-
-    return {dados, carregando}
+    console.log(urlDjango)
+    
+    fetch(urlDjango, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken':  getCookies('csrftoken'),
+        },
+        body: payloadString,
+        credentials: 'include',
+    })
+    .then(response => response.json())
+    .then(data => {
+        return data
+    })
+    .catch (error => {
+        console.error('Erro na requisição: ', error)
+    })
 }
 
 export default useRequestDjango
