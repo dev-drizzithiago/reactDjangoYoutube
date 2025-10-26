0d7c997c67f84f8b13bea1d29ec0fef187132311
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-25 22:30:21 -0300

    Update App.js

--- React/youtube/src/App.js
+++ React/youtube/src/App.js
@@ -5,27 +5,29 @@ import FormularioLinkYoutube from './componentes/FormularioLinkYoutube';
 import LinkBancoDados from './componentes/LinkBancoDados';
 import useCsrfInit from './componentes/useCsrfInit';
 
+import { useState } from 'react';
+
 function App() {
   {/**- Tudo fora do return (dentro da função do componente) 
     é onde você coloca lógica, hooks, variáveis, chamadas de API, etc.
  */}
 
+  /** Recebe um GET do django com o cookies */
   useCsrfInit();
-  {/** - Tudo dentro do return é JSX, ou seja, a estrutura visual que será renderizada na tela.*/}
+  {/** - Tudo dentro do return é JSX, ou seja, a 
+    estrutura visual que será renderizada na tela.*/}
+
+  const [responseFormulario, setResponseFormulario] = useState(0)
   
   return (
     <div className="App">
-      <div className='logoPrincipal'>
-        <LogoYoutube />        
-      </div>
-
       <div className='divPgPrincipal formsInputLink'>
-        <FormularioLinkYoutube />
+        <FormularioLinkYoutube  onLinkAdicionado={() => setResponseFormulario(prev => prev + 1)} />
       </div>
 
       <div className='containePrincipal'>
         <div className='divPgPrincipal viewLinksyoutube'>
-          <LinkBancoDados />
+          <LinkBancoDados atualizar={responseFormulario} />
         </div>
       </div>
     </div>
