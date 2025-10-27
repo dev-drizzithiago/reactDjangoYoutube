726ec125716cbec0093b78a39a36e169b388b4b2
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-10-26 21:02:13 -0300

    Update App.js

--- React/youtube/src/App.js
+++ React/youtube/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import FormularioLinkYoutube from './componentes/FormularioLinkYoutube';
 import LinkBancoDados from './componentes/LinkBancoDados';
 import useCsrfInit from './componentes/useCsrfInit';
-
 import { useEffect, useState} from 'react';
 
 import useRequestDjango from './componentes/useRequestDjango';
@@ -18,15 +17,10 @@ function App() {
   {/** - Tudo dentro do return é JSX, ou seja, a 
     estrutura visual que será renderizada na tela.*/}
 
-  const [dadosRequestLinks, setDadosRequestLinks] = useState(null);
-  const linksBaseDados = "http://localhost:8000/requestBaseDados/";
-  
-  const dados = useRequestDjango(linksBaseDados, {dados: 'Listar links'})
-  setDadosRequestLinks(dados)
   return (
     <div className="App">
-        <FormularioLinkYoutube />      
-        <LinkBancoDados dados={dadosRequestLinks}/>
+        <FormularioLinkYoutube />
+        <LinkBancoDados />
     </div>
   );
 }
