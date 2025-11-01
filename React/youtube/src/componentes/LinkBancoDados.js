16aa0b4e46fc818ad08a68006d52c3e0915d84be
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-01 13:28:38 -0300

    Update LinkBancoDados.js

--- React/youtube/src/componentes/LinkBancoDados.js
+++ React/youtube/src/componentes/LinkBancoDados.js
@@ -6,36 +6,45 @@ import "./LinkBancoDados.css"
 import { useState } from "react";
 
 const LinkBancoDados = ({triggerAtualizacao}) => {
-    const [SendDadosDownload, setSendDadosDownload] = useState(null)
+    const [downloadMidias, setdownloadMidias] = useState(false)
 
     const {dados, carregando} = useRequestDjango("http://localhost:8000/requestBaseDados/", 'Listar', triggerAtualizacao)
+
     if (carregando) return <img src="/img/imgBtns/loading.gif" alt="Carregando..."/>
 
-    const downloadVideoAndMusic = async (link_tube, id_dados) => {
+    /** Função para preparar o download tanto em video como em musicas mp3 */
+    const downloadVideoAndMusic = async (id_dados) => {
         const dadosDownload = {
             id_dados: id_dados,
-            midia: 'MP4',
+            midia: 'MP3',
         }
-
-        console.log(dadosDownload)
+        setdownloadMidias(true)
         const djangoUrlDownloads = "http://localhost:8000/download_link/"
         const responseDjangoDownload = await sendRequestDjango(djangoUrlDownloads, dadosDownload)
 
         console.log(responseDjangoDownload)
+        setdownloadMidias(false)
     }
 
-    const removeLinkBaseDados = (link_tube) => {
+    const removeLinkBaseDados = async (id_dados) => {
+        const dadosDownload = {
+            id_dados: id_dados,            
+        }
+        const djangoUrlDownloads = "http://localhost:8000/remove_link/"
+        const responseDjangoDownload = await sendRequestDjango(djangoUrlDownloads, dadosDownload)
 
+        console.log(responseDjangoDownload)
     }
 
     return (
         <div>
             <h1> Links para download </h1>            
             <div className="content">
-                {dados.map((item, index) => (
+                
+                {dados.map((item) => (
                     <div className="linksYoutube" key={item.id_dados}>         
                         <div>
-                            <p>{item.id_dados}</p> <p>{item.autor_link}</p> <p>{item.titulo_link}</p>
+                            <p>{item.autor_link}</p> <p>{item.titulo_link}</p>
                         </div>
 
                         <div className="divImgMiniatura">
@@ -44,14 +53,14 @@ const LinkBancoDados = ({triggerAtualizacao}) => {
                         
                         <p className="paragraphTitulos">                            
                             
-                        <img src="/img/imgBtns/download.png" alt="download" className="imgBtn imgBtnDownload" onClick={() => downloadVideoAndMusic(item.link_tube, item.id_dados)}  />
+                        <img src="/img/imgBtns/download.png" alt="download" className="imgBtn imgBtnDownload" onClick={() => downloadVideoAndMusic(item.id_dados)}  />
                     
                         <img src="/img/imgBtns/remover.png" alt="remover" className="imgBtn imgBtnRemover" onClick={() => removeLinkBaseDados(item.id_dados) } />
 
-                        <a href={item.link_tube} target="_blank"><img src="/img/imgBtns/youtube.png" alt="link" className="imgBtn imgBtnLink"/></a>
-                        
+                        <a href={item.link_tube} target="_blank"><img src="/img/imgBtns/youtube.png" alt="link" className="imgBtn imgBtnLink"/></a>                        
                             
                         </p>
+                        {downloadMidias && <div className="imgLoading"><img src="/img/imgBtns/loading.gif" alt="Carregando..."/></div>}
                     </div>                                
                 ))}                
             </div>
