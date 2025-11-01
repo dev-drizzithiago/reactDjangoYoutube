b8e314a0eab1b095c1fec372d58ae634b38dff64
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-01 11:25:08 -0300

    Update urls.py

--- Django/coreYoutube/urls.py
+++ Django/coreYoutube/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, requestBaseDados, requestAddLinks, csrf_token_view
+from .views import index, requestBaseDados, requestAddLinks, csrf_token_view, download_videos
 
 urlpatterns = [
     path('index', index, name='index'),
     path('csrf_token_view/', csrf_token_view, name='csrf_token_view'),
     path('requestBaseDados/', requestBaseDados, name='requestBaseDados'),
     path('requestAddLinks/', requestAddLinks, name='requestAddLinks'),
+    path('download_videos/', download_videos, name='download_videos'),
 ]
