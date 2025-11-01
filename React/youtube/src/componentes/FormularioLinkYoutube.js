92d7e7c19caeea9a5285e315d06336ab8c820410
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-01 16:34:05 -0300

    Update FormularioLinkYoutube.js

--- React/youtube/src/componentes/FormularioLinkYoutube.js
+++ React/youtube/src/componentes/FormularioLinkYoutube.js
@@ -5,44 +5,37 @@ import "./FormularioLinkYoutube.css"
 
 
 const FormularioLinkYoutube = ({ onLinkAdicionado }) => {
-    const [btnLimparForms, setLimparForms] = useState(false)
+    const [btnLimparForms, setLimparForms] = useState(null)
     const [responseAlertaDjango, setResponseAlertaDjango] = useState(null)
     const [carregando, setCarregando] = useState(null)
     const [atualizarLinks, setAtualizeLinks] = useState(false)
 
     const refLink = useRef();
 
-    
     /** FUNÇÃO PARA ADICIONAR LINK */
-
     const useDefGravandoLink = async () => {
-      setLimparForms(true)
-      setCarregando(true)
       const linkYoutube = refLink.current.value
-
-      if (btnLimparForms) {
-        refLink.current.value = '';     
-       }
+      refLink.current.value = '';
+      setCarregando(true)
 
       const responseDados = await sendRequestDjango("http://localhost:8000/requestAddLinks/", {'link': linkYoutube})
       
       setResponseAlertaDjango(responseDados.mensagem);
       setCarregando(false)
 
-      {atualizarLinks && <LinkBancoDados />}
-
-      if (responseDados.erro_processo === 0){
-
+      /** Se não ocorrer nenhum erro, a lista de links serão atualizadas. */
+      if (responseDados.erro_processo === 0) {
         onLinkAdicionado() /** Comunica o app.js que deve atualizar os links. */
       }
       
+      /** Depois de 15 segundos, a mensagem de alerta vai ser removida. */
       setTimeout(() => {
           setResponseAlertaDjango('')
         }, 15000);        
     }
     
     /** FUNÇÃO PARA LIMPAR O CAMPO DE LINK */
-    const useDefBtnLimparInput = () => {      
+    const useDefBtnLimparInput = () => {
       if (refLink.current) {
         refLink.current.value = '';
         setResponseAlertaDjango('')
@@ -55,17 +48,17 @@ const FormularioLinkYoutube = ({ onLinkAdicionado }) => {
         <div className="divLabelInput">
           <label className="label labelLink" htmlFor="link">Cole o Link do Video:</label>
           <input type="text" className="inputText inputLinkYoutube" name="link" ref={refLink}/>
-        </div>
-
-        <div>                   
-          {carregando && <img src="/img/imgBtns/loading.gif" alt="Carregando..."/>}
-          <h3 className="mensagemAlerta">{responseAlertaDjango}</h3>
-        </div>
+        </div>       
         
         <div className="divBtnImgAdd">
           <img src="/img/imgBtns/adicionar.png" alt="adicionar" className="imgBtn btnAdd" onClick={useDefGravandoLink}/>
-          <img src="/img/imgBtns/limpar.png" alt="adicionar" className="imgBtn btnLimpar" onClick={useDefBtnLimparInput}/>
-        </div>        
+          <img src="/img/imgBtns/limpar.png" alt="adicionar" className="imgBtn btnLimpar" onClick={useDefBtnLimparInput}/>          
+          {carregando && <div className="divImgLoading"><img  className="imgBtn imgLoading" src="/img/imgBtns/spinner.gif" alt="Carregando..."/></div>}
+        </div>   
+
+        <div>          
+          <h3 className="mensagemAlerta">{responseAlertaDjango}</h3>
+        </div>     
     </div>
    
   );
