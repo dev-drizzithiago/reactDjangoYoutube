d49de7956991a540be48bbff9b9b044a2a9f0f85
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-02 20:18:13 -0300

    Update PlayerMidiasMp3.js

--- React/youtube/src/componentes/PlayerMidiasMp3.js
+++ React/youtube/src/componentes/PlayerMidiasMp3.js
@@ -8,16 +8,17 @@ const PlayerMidiasMp3 = ({ effectAtualizacao }) => {
         tipoMidia: 'MP3',
     }
 
-    const {responseDjango, carregando} = useRequestDjango("http://localhost:8000/listagem_midias/", payload, effectAtualizacao)
+    const {dados, carregando} = useRequestDjango("http://localhost:8000/listagem_midias/", payload, effectAtualizacao)
 
-    console.log(responseDjango)
+    console.log(dados)
 
     if (carregando) return <img src="/img/imgBtns/loading.gif" alt="Carregando..."/>
 
   return (
     <div>
-        {responseDjango.map((item, index) => (
-            <div className="paragraphTitulos" key={index.id_music}>
+        <h3>Lista MP3</h3>
+        {dados.map((item) => (
+            <div className="paragraphTitulos" key={item.id_music}>
                 <p>{item.nome_arquivo}</p> <p>{item.duracao_midia}</p>
             </div>
         ))}
