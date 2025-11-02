4eccb04e87daa7bd917630a08b740e36c19ee185
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-02 13:57:23 -0300

    Update PlayerMidiasMp3.js

--- React/youtube/src/componentes/PlayerMidiasMp3.js
+++ React/youtube/src/componentes/PlayerMidiasMp3.js
@@ -1,12 +1,18 @@
 /**  key= só utilizando que a lista não possui uma chave. */
 
-const PlayerMidiasMp3 = () => {
+import { useState } from "react";
 
-    
+const PlayerMidiasMp3 = () => {
+    const [dadosMidiasLista, setDados] = useState([])
+
 
   return (
     <div>
-        
+        {dadosMidiasLista.map((item) => (
+            <div className="paragraphTitulos">
+                <p>{item.autor_link}</p> <p>{item.titulo_link}</p>
+            </div>
+        ))}
     </div>
   );
 };
