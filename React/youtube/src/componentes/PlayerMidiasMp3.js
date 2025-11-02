6431f952bf2b08aac695e144b7189e0daa2d4e64
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-02 12:42:14 -0300

    Update PlayerMidiasMp3.js

--- React/youtube/src/componentes/PlayerMidiasMp3.js
+++ React/youtube/src/componentes/PlayerMidiasMp3.js
@@ -1,36 +1,12 @@
 /**  key= só utilizando que a lista não possui uma chave. */
 
-import { useState } from "react";
-
 const PlayerMidiasMp3 = () => {
 
-    const [listaMp3Salva] = useState(
-        [{
-            nome_midia: 'Nome_1',
-            link_midia: '../Django/medias/MP4/Black TBlack T - Breve.mp4',
-            miniatura: ''
-        }]
-    )
+    
 
   return (
     <div>
-        <div className="listaMidias">
-            <ul>
-                {listaMp3Salva.map(
-                    (item, index) => (                        
-                    <a href="item.link_midia">{item.nome_midia}</a>                        
-                    )
-                )}
-            </ul>
-        </div>
-
-        <div className="modalPlayVideos">
-                <div class="div div_dialog_play">
-                    <video controls class="video_player">
-                        <source src="../Django/medias/MP4/Black TBlack T - Breve.mp4" type="video/mp4"></source>
-                    </video>
-                </div>
-        </div>
+        
     </div>
   );
 };
