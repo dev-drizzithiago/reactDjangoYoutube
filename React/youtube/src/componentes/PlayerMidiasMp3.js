5998aa64bcccd570d40a6b32835a1c3565cb0a51
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-02 20:51:42 -0300

    Update PlayerMidiasMp3.js

--- React/youtube/src/componentes/PlayerMidiasMp3.js
+++ React/youtube/src/componentes/PlayerMidiasMp3.js
@@ -3,25 +3,31 @@
 import useRequestDjango from "./useRequestDjango";
 
 const PlayerMidiasMp3 = ({ effectAtualizacao }) => {    
-    
+    const urlMiniatura = "http://localhost:8000/"
     const payload = {
         tipoMidia: 'MP3',
     }
 
     const {dados, carregando} = useRequestDjango("http://localhost:8000/listagem_midias/", payload, effectAtualizacao)
 
-    console.log(dados)
-
     if (carregando) return <img src="/img/imgBtns/loading.gif" alt="Carregando..."/>
 
   return (
-    <div>
-        <h3>Lista MP3</h3>
-        {dados.map((item) => (
-            <div className="paragraphTitulos" key={item.id_music}>
-                <p>{item.nome_arquivo}</p> <p>{item.duracao_midia}</p>
-            </div>
-        ))}
+    <div className="divPrincipal">        
+        <div className="content">
+            <h3>Lista MP3</h3>
+                {dados.map((item) => (
+                    <div>
+                        <div className="paragraphTitulos" key={item.id_music}>
+                            <p>{item.nome_arquivo}</p> <p>{item.duracao_midia}</p>
+                        </div>
+                        <div>
+                            <p>{`${urlMiniatura}${item.path_miniatura}`}</p>
+                            <img src={`${urlMiniatura}${item.path_miniatura}`} alt="" />
+                        </div>
+                    </div>                     
+                ))}            
+        </div>        
     </div>
   );
 };
