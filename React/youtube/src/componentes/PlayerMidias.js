ebbe36e6fd40bb61cbb68ff92902b043a0770340
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-16 19:22:23 -0300

    Update PlayerMidias.js

--- React/youtube/src/componentes/PlayerMidias.js
+++ React/youtube/src/componentes/PlayerMidias.js
@@ -1,14 +1,23 @@
-
-export const PlayerMidias = ({executandoMidia}) => {
+import './PlayerMidias.css'
+export const PlayerMidias = ({executandoMidia, fecharPlayer}) => {
 
     console.log(executandoMidia)
+
+    const fecharPlayerMidia = () => {
+      console.log('Fechar Player')
+      fecharPlayer(0)
+    }
 
   return (
     <div>
         <h1> PLAYER DE MIDIAS</h1>
         <video width="500px" height="600px">
             <source src={executandoMidia[0]} type={executandoMidia[1]}/>
-        </video>        
+        </video>
+        
+
+        <img src="/img/imgBtns/botao-fechar.png" alt="player" className="player-midia-imgBtn" onClick={fecharPlayerMidia} />
+        
     </div>
   )
 }
