1698a5cfe71cbb10a29620b4fa3aaa94f4efaf62
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-20 12:11:26 -0300

    Update urls.py

--- Django/coreYoutube/urls.py
+++ Django/coreYoutube/urls.py
@@ -7,7 +7,7 @@ from .views import (
     download_link,
     remove_link,
     listagem_midias,
-    download_midias,
+    preparar_midias_to_download,
 )
 
 from django.conf import settings
@@ -21,7 +21,7 @@ urlpatterns = [
     path('download_link/', download_link, name='download_link'),
     path('remove_link/', remove_link, name='remove_link'),
     path('listagem_midias/', listagem_midias, name='listagem_midias'),
-    path('download_midias/', download_midias, name='download_midias'),
+    path('preparar_midias_to_download/', preparar_midias_to_download, name='preparar_midias_to_download'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
