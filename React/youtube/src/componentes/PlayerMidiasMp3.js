17c78a58da0979c24b0c792161985f36905fd58f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-20 18:34:07 -0300

    Update PlayerMidiasMp3.js

--- React/youtube/src/componentes/PlayerMidiasMp3.js
+++ React/youtube/src/componentes/PlayerMidiasMp3.js
@@ -36,7 +36,10 @@ const PlayerMidiasMp3 = ({ effectAtualizacao, executaMidia }) => {
         }
 
         const response = await sendRequestDjango("http://localhost:8000/preparar_midias_to_download/", payload);
-        if (response && response.download_url)
+        if (response && response.download_url) {
+            const url = `http://localhost:8000${response.download_url}`
+            window.open(url, "_blank");
+        }
    }
 
     const removeDeleteMidia = () => {
