0851fe5020d32a5021757bc065c73ce3e22f4389
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-21 20:02:35 -0300

    Update LoginUsuario.js

--- React/youtube/src/componentes/LoginUsuario.js
+++ React/youtube/src/componentes/LoginUsuario.js
@@ -1,3 +1,4 @@
+import './LoginUsuario.css'
 
 const LoginUsuario = () => {
 
@@ -5,6 +6,26 @@ const LoginUsuario = () => {
   return (
     <div>
         <h1>Login</h1>
+        <div className='login-divInputs'>
+          <label htmlFor="usuario: ">
+            Usuário<input type="text" name='usuario' className='login-input login-inputUsuario'/>
+          </label>
+
+          <label htmlFor="senha: ">
+            Senha<input type="password" name='senha' className='login-input login-inputSenha'/>
+          </label>
+        </div>        
+
+        <div className=''>
+          <a href="">
+            <img className="login-btnNewUser" src="/img/imgBtns/adicionar-usuario.png" alt="" />
+          </a>
+          <a href="">
+            <img className="login-btnNewUser" src="/img/imgBtns/verificar.png" alt="" />
+          </a>
+        </div>
+        
+        
     </div>
   )
 }
