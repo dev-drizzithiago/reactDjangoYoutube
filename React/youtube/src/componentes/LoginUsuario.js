f7ac36d3a0af3feccd19bb635cd1c18dead28be9
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-22 20:46:52 -0300

    Update LoginUsuario.js

--- React/youtube/src/componentes/LoginUsuario.js
+++ React/youtube/src/componentes/LoginUsuario.js
@@ -5,12 +5,19 @@ const LoginUsuario = () => {
   const [criarUser, setCriarUser] = useState(false)
   const [btnCriarNovoUserAtivo, setBtnCriarNovoUserAtivo] = useState(true)
 
+  const [dadosUser, setDadosUser] = useState([])
+
   const criarNovoUsuario = () => {
     setBtnCriarNovoUserAtivo(false)
     setCriarUser(true)
   }
   
   const salvarNovoUser = () => {
+    const PAYLOAD = {
+      'tipoRequest': 'salvarCadastro',
+      'dadosNovaCredencia': ''
+    }
+
     setBtnCriarNovoUserAtivo(true)
     if (criarUser) {
       setCriarUser(false)
@@ -25,22 +32,35 @@ const LoginUsuario = () => {
             <h3>Cadastro</h3>
             <label htmlFor="nomeCompleto: ">
               Nome Completo
-              <input type="text" name='nomeCompleto' className='login-input login-inputNomecompleto'/>
+              <input type="text" name='nomeCompleto' className='login-input login-inputNomecompleto' />
             </label>
+
             <label htmlFor='email'>
               E-mail
               <input type="email" name='email' className='login-input login-inputEmail' />
             </label>
+
+            <label htmlFor='senha'>
+              Password
+              <input type="password" name='senha' className='login-input login-inputSenha' />
+            </label>
+
+            <label htmlFor='confirm-senha'>
+              Confirmar Password
+              <input type="password" name='confirm-senha' className='login-input login-inputConfirSenha' />
+            </label>
             <img className="login-btnNewUser" src="/img/imgBtns/salve.png" alt="" onClick={salvarNovoUser}/>
           </div>}
 
-          <div className='login-divLogin'>
+          {!criarUser && <div className='login-divLogin'>
             <h3>Login</h3>
             <label htmlFor="usuario: ">
-              Usuário<input type="text" name='usuario' className='login-input login-inputUsuario'/>
+              Usuário
+              <input type="text" name='usuario' className='login-input login-inputUsuario'/>
             </label>
             <label htmlFor="senha: ">
-              Senha<input type="password" name='senha' className='login-input login-inputSenha'/>
+              Senha
+              <input type="password" name='senha' className='login-input login-inputSenha'/>
             </label>          
               <img className="login-btnNewUser login-btnVerificar" src="/img/imgBtns/verificar.png" alt="" />
               {
@@ -48,7 +68,7 @@ const LoginUsuario = () => {
                 <img className="login-btnNewUser login-btnCriarNovoUser" 
                 src="/img/imgBtns/adicionar-usuario.png" alt="criar usuario" onClick={criarNovoUsuario} /> 
               }          
-          </div>
+          </div>}
         </div>
     </div>
   )
