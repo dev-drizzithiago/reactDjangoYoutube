bb1d41780c6385d09fd0ecbd1f77e2a40464c62d
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-12-05 14:49:13 -0300

    Update VerificarUsuarioLogado.js

--- React/youtube/src/componentes/VerificarUsuarioLogado.js
+++ React/youtube/src/componentes/VerificarUsuarioLogado.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
-import sendRequestDjango from './componentes/sendRequestDjango';
+import sendRequestDjango from './sendRequestDjango'
 
-const VerificarUsuarioLogado = () => {
+const VerificarUsuarioLogado = ({}) => {
 
     const [statusLogin, setStatusLogin] = useState(false)
 
@@ -10,35 +10,29 @@ const VerificarUsuarioLogado = () => {
         console.log('Usuario Logado: ', statusLogin)
 
         const verificarStatusLogin = async () => {
-        if (!statusLogin) {  // Se o usuario estiver deslogado continua o processo
-            const PAYLOAD = {
-                'tipoRequest': 'verificarUsuarioLogado',
+            if (!statusLogin) {  // Se o usuario estiver deslogado continua o processo
+                const PAYLOAD = {
+                    'tipoRequest': 'verificarUsuarioLogado',
+                }
+                const urlDjangoLogin = `http://localhost:8000/credenciais_login/`;
+                const responseDjango = await sendRequestDjango(urlDjangoLogin, PAYLOAD)
+                
+                if (responseDjango.usuario_logado) 
+                    {
+                    setStatusLogin(true)
+
+                } else if (!responseDjango.usuario_logado){
+
+                    setStatusLogin(false)
+                }
             }
-            const urlDjangoLogin = `http://localhost:8000/credenciais_login/`;
-            const responseDjango = await sendRequestDjango(urlDjangoLogin, PAYLOAD)
-            
-            console.log(responseDjango)
-
-            if (responseDjango.usuario_logado) 
-                {
-                setStatusLogin(true)
-
-            } else if (!responseDjango.usuario_logado){
-
-                setStatusLogin(false)
-            }
-        }
         }
 
         verificarStatusLogin()
 
   }, [])
 
-  return (
-    <div>
-        
-    </div>
-  )
+  return statusLogin
 }
 
 export default VerificarUsuarioLogado
