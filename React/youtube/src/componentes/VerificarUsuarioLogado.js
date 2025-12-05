cdd9998f720d8fba8e09e132daa3b317747c84e4
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-12-05 11:44:33 -0300

    Create VerificarUsuarioLogado.js

---
+++ React/youtube/src/componentes/VerificarUsuarioLogado.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const VerificarUsuarioLogado = () => {
+
+    /** Verifica se o usuário esta logado. */
+    useEffect(() => {
+        console.log('Usuario Logado: ', statusLogin)
+
+        const verificarStatusLogin = async () => {
+        if (!statusLogin) {  // Se o usuario estiver deslogado continua o processo
+            const PAYLOAD = {
+                'tipoRequest': 'verificarUsuarioLogado',
+            }
+            const urlDjangoLogin = `http://localhost:8000/credenciais_login/`;
+            const responseDjango = await sendRequestDjango(urlDjangoLogin, PAYLOAD)
+            
+            console.log(responseDjango)
+
+            if (responseDjango.usuario_logado) {
+            setStatusLogin(true)
+            } else if (!responseDjango.usuario_logado){
+            setStatusLogin(false)
+            }
+        }
+        }
+
+        verificarStatusLogin()
+
+  }, [])
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default VerificarUsuarioLogado
