e697e1575c18d3e7ff431155bb38e1f9fae9143c
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-12-17 09:12:26 -0300

    Update useRequestDjango.js

--- React/youtube/src/componentes/useRequestDjango.js
+++ React/youtube/src/componentes/useRequestDjango.js
@@ -22,7 +22,9 @@ const useRequestDjango = (urlDjango, payload, trigger) => {
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+
+            console.log(data.erro_processo)
+            
             if (data.erro_processo === 666 ) {
                 setDados(data.dados_django);
                 setCarregando(false);
