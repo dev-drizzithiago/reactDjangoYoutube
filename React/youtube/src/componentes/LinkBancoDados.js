2a132aa852e64718f037205d5a61383d21280fe8
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-12-17 09:22:21 -0300

    Update LinkBancoDados.js

--- React/youtube/src/componentes/LinkBancoDados.js
+++ React/youtube/src/componentes/LinkBancoDados.js
@@ -9,8 +9,8 @@ import "./LinkBancoDados.css"
 
 import { useState, useEffect } from "react";
 
-const LinkBancoDados = ({propsStatusProcesso}) => {    
-    console.log('Chamando componente')
+const LinkBancoDados = ({propsStatusProcesso}) => {
+
     const [atualizacaoBaseLinks, setAtualizacaoBaseLinks] = useState(0);
     const [downloadMidias, setdownloadMidias] = useState(null);
     const [statusLogin, setStatusLogin] = useState(null);
