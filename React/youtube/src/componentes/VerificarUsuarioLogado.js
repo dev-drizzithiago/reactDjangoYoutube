5a9427f0743cf50698ea54a8e0c80da53794f2d6
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-12-18 17:53:18 -0300

    Update VerificarUsuarioLogado.js

--- React/youtube/src/componentes/VerificarUsuarioLogado.js
+++ React/youtube/src/componentes/VerificarUsuarioLogado.js
@@ -1,34 +1,23 @@
-import { useState, useEffect } from 'react';
 import sendRequestDjango from './sendRequestDjango'
 
-const VerificarUsuarioLogado = ({}) => {
 
-    const [statusLogin, setStatusLogin] = useState(false)
-    console.log('Usuario Logado: ', statusLogin)
-
-    /** Verifica se o usuário esta logado. */
-    useEffect(() => {
-
-        const verificarStatusLogin = async () => {
-            if (!statusLogin) {  // Se o usuario estiver deslogado continua o processo
-                const PAYLOAD = {
-                    'tipoRequest': 'verificarUsuarioLogado',
-                }
-                const urlDjangoLogin = `http://localhost:8000/credenciais_login/`;
-                const responseDjango = await sendRequestDjango(urlDjangoLogin, PAYLOAD)
-                
-                if (responseDjango.usuario_logado) 
-                    {
-                    setStatusLogin(true)
-
-                } else if (!responseDjango.usuario_logado){
-                    setStatusLogin(false)
-                }
-            }
+const verificarStatusLogin = async () => {
+    if (!statusLogin) {  // Se o usuario estiver deslogado continua o processo
+        const PAYLOAD = {
+            'tipoRequest': 'verificarUsuarioLogado',
         }
-        verificarStatusLogin()
-  }, [])
-  return statusLogin
+        const urlDjangoLogin = `http://localhost:8000/credenciais_login/`;
+        const responseDjango = await sendRequestDjango(urlDjangoLogin, PAYLOAD)        
+        if (responseDjango.usuario_logado) {
+            console.log(responseDjango.usuario_logado)
+        } else if (!responseDjango.usuario_logado){
+            console.log(responseDjango.usuario_logado)
+        }
+    }
+    return 'statusLogin'
 }
 
-export default VerificarUsuarioLogado
+export default verificarStatusLogin
+
+
+
