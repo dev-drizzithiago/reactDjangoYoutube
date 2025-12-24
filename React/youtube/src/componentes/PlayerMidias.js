f3f1e56bfbfc4f322603a30e8fce16d8e26e4198
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-12-24 08:03:32 -0300

    Update PlayerMidias.js

--- React/youtube/src/componentes/PlayerMidias.js
+++ React/youtube/src/componentes/PlayerMidias.js
@@ -6,13 +6,13 @@ const PlayerMidias = ({executandoMidia, fecharPlayer}) => {
       fecharPlayer()
     }
 
-    console.log(`http://localhost:8000/media/${executandoMidia[0]}`)
+    console.log(encodeURI(`http://localhost:8000/media/${executandoMidia[0]}`))
 
     return (
       <div>
           <h1> PLAYER DE MIDIAS</h1>
-            <video controls width="500px" height="600px" className='playervideo'>
-                <source src={`http://localhost:8000/media/${executandoMidia[0]}`} type={executandoMidia[1]}/>
+            <video controls className='playerMidias'>
+                <source src={encodeURI(`http://localhost:8000/media/${executandoMidia[0]}`)} type={executandoMidia[1]}/>
             </video>
 
           <img src="/img/imgBtns/botao-fechar.png" alt="player" className="player-midia-imgBtn" onClick={fecharPlayerMidia} />
