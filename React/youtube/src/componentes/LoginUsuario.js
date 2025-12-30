67559224df4f6eb27cf319b33f815109e13523f4
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-12-30 11:00:26 -0300

    Update LoginUsuario.js

--- React/youtube/src/componentes/LoginUsuario.js
+++ React/youtube/src/componentes/LoginUsuario.js
@@ -69,7 +69,9 @@ const LoginUsuario = ({infoStatusLogin}) => {
         },
       }
       const responseDjango = await sendRequestDjango(linkSendRequest, PAYLOAD)
+
       console.log(responseDjango)
+      
       if (responseDjango.erro_processo !== 1) {
         if (responseDjango.nome_usuario === 'AnonymousUser'){
           console.log(responseDjango.mensagem_erro)
