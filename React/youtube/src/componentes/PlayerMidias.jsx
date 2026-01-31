de980659524473e4ce8a21ac6fa82a8158a98431
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-01-31 18:15:56 -0300

    Update PlayerMidias.jsx

--- React/youtube/src/componentes/PlayerMidias.jsx
+++ React/youtube/src/componentes/PlayerMidias.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './PlayerMidias.css'
 
+const urlDefaultDjango = "http://localhost:8000"
 
 const PlayerMidias = ({executandoMidia, fecharPlayer}) => {
     const fecharPlayerMidia = () => {
