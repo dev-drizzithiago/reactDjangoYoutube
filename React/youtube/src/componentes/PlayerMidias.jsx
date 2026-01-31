82b0f29e768e0bd5ac5532e5f742f16f28503ad8
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-01-31 18:20:30 -0300

    Update PlayerMidias.jsx

--- React/youtube/src/componentes/PlayerMidias.jsx
+++ React/youtube/src/componentes/PlayerMidias.jsx
@@ -12,7 +12,7 @@ const PlayerMidias = ({executandoMidia, fecharPlayer}) => {
       <div>
           <h1> PLAYER DE MIDIAS</h1>          
             <video controls className='playerMidias'>
-                <source src={encodeURI(`http://localhost:8000/media/${executandoMidia[0]}`)} type={`${executandoMidia[1]}`}/>
+                <source src={encodeURI(`${urlDefaultDjango}/media/${executandoMidia[0]}`)} type={`${executandoMidia[1]}`}/>
             </video>
           <img src="/img/imgBtns/botao-fechar.png" alt="player" className="player-midia-imgBtn" onClick={fecharPlayerMidia} />
       </div>
