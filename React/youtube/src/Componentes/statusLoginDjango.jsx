6814e99afeab6d37327e5f35c53dbdd1b8febdbc
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-05 21:08:16 -0300

    Update statusLoginDjango.jsx

--- React/youtube/src/Componentes/statusLoginDjango.jsx
+++ React/youtube/src/Componentes/statusLoginDjango.jsx
@@ -2,7 +2,7 @@ import sendRequestDjango from "./sendRequestDjango";
 
 export const verificarUsuarioLogado = async (linkSendRequest, PAYLOAD) => {
     const responseDjango = await sendRequestDjango(linkSendRequest, PAYLOAD)
-    return responseDjango
+    return responseDjango.usuario_logado
 }
 
 export const logarUsuario = async (linkSendRequest, PAYLOAD) => {
@@ -14,5 +14,3 @@ export const deslogarUsuario = async (linkSendRequest, PAYLOAD) => {
     const responseDjango = await sendRequestDjango(linkSendRequest, PAYLOAD)
     return responseDjango
 }
-
-export default logarUsuario
