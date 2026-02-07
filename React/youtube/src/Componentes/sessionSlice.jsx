462776466535df80ac50181de7c4bb685049f447
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-07 11:40:47 -0300

    Update sessionSlice.jsx

--- React/youtube/src/Componentes/sessionSlice.jsx
+++ React/youtube/src/Componentes/sessionSlice.jsx
@@ -2,7 +2,18 @@ import { createSliece } from '@reduxjs/toolkit';
 
 const sessionSlice = createSliece({
     nome: 'session',
-    initialState: {
-        usuario: null, logado: false
-    }
-})
+    initialState: { usuario: null, logado: false },
+    reducers: {
+        loginSuccess(state, action) {
+            state.usuario = action.payload;
+            state.logado = true;
+        },
+        logout(state) {
+            state.usuario = null;
+            state.logado = false;
+        },
+    },
+});
+
+export const { loginSuccess, logout } = sessionSlice.actions;
+export default sessionSlice.reducers;
