5afad8d17f3f9178bc40c0415d4304bd66218474
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-07 11:53:26 -0300

    Update store.jsx

--- React/youtube/src/Componentes/store.jsx
+++ React/youtube/src/Componentes/store.jsx
@@ -1,5 +1,11 @@
-import { consigureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { persistStore } from 'redux-persist';
 
 import storageSession from 'redux.persist/lib/storage/session';
 import sessionReducer from './sessionSlice';
+
+const persistConfig = {key: 'root', storage: storageSession};
+const persistedReducer = persistedReducer(persistConfig, sessionReducer);
+
+export const store = configureStore({reducer: {session: persistedReducer} });
+export const persistor = persistStore(store)
