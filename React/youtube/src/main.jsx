bb7a9f2c8cacb0a96f22aa88d68b8c96c2d66341
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-08 16:39:59 -0300

    Update main.jsx

--- React/youtube/src/main.jsx
+++ React/youtube/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import storageSession from "redux-persist/lib/storage/session";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import App from "./App.jsx";
+import { store, persistor } from "./Componentes/store.jsx"; // importa o store que você configurou
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
