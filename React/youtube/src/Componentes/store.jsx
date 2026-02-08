0f4e1b6ddc4c1c1db53889465abd6253e9308e6e
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-08 16:39:57 -0300

    Update store.jsx

--- React/youtube/src/Componentes/store.jsx
+++ React/youtube/src/Componentes/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
-import storageSession from 'redux.persist/lib/storage/session';  // - storageSession → usa o sessionStorage do navegador para guardar o estado.
+import storageSession from "redux-persist/lib/storage/session";  // - storageSession → usa o sessionStorage do navegador para guardar o estado.
 import sessionReducer from './sessionSlice';  // sessionReducer → o reducer que criamos no bloco 1.
 
 // - key: "root" → chave usada para salvar no storage.  
@@ -9,7 +9,7 @@ const persistConfig = {key: 'root', storage: storageSession};
 
 //- Cria um reducer “persistido”: combina o reducer original 
 // com a lógica de salvar/carregar do storage.
-const persistedReducer = persistedReducer(persistConfig, sessionReducer);
+const persistedReducer = persistReducer(persistConfig, sessionReducer);
 
 //- store → cria o Redux store com o reducer persistido.
 export const store = configureStore({reducer: {session: persistedReducer} });
