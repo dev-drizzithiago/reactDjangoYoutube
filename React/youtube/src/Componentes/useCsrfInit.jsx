ec84446334cc04631b72af09520c12290c233b82
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-13 20:45:12 -0300

    Update useCsrfInit.jsx

--- React/youtube/src/Componentes/useCsrfInit.jsx
+++ React/youtube/src/Componentes/useCsrfInit.jsx
@@ -1,18 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch } from 'react-redux';
+
+// - Importa as actions criadas no slice.
+import { loginSuccess, logout } from "./sessionSlice";
 
 const urlDefaultDjango = `http://localhost:8080`
 
 const useCsrfInit = () => {
 
     const urlDjango =`${urlDefaultDjango}/csrf_token_view/`
+    const [statusLogin, setStatusLogin] = useState(false)
+
+    // - Cria dispatch para enviar ações.
+    const dispatch = useDispatch()
     
     useEffect(()=>{
         fetch(urlDjango, {
             method: 'GET',
             credentials: 'include', // Recebe o cookies do django
         })
-        .then(() => {
-            //console.log('CSRF cookie recebido');
+        .then(data => data.json())
+        .then((data) => {
+            console.log('CSRF cookie recebido');
+            console.log(data);
+
+            if (data.user_logado) {
+                dispatch(loginSuccess(data.user_logado))
+            } else {
+                dispatch(logout())
+            }
         })
         .catch(err => {
             console.error("Erro ao obter CSRF cookie: ", err)
