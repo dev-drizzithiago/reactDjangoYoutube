597a1e7d76d6d49191af2de568722387541cacd6
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-18 13:30:23 -0300

    Update settings.py

--- Django/DjangoYouTube/settings.py
+++ Django/DjangoYouTube/settings.py
@@ -54,14 +54,12 @@ MIDDLEWARE = [
 
 # - Permitir que navegadores façam requisições cross-origin
 CORS_ALLOWED_ORIGINS = [
-    'http://localhosta:3000',
-    'http://192.168.15.254:3000'
+    'http://localhost:3000',
 ]
 
 # - Permitir que o Django aceite requisições com cookies e tokens CSRF vindas de domínios confiáveis
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhosta:3000',
-    'http://192.168.15.254:3000'
+    'http://localhost:3000',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
