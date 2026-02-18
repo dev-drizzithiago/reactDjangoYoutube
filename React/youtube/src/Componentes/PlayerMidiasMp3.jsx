57a01176a777c1e17f5736ab93dd7cd4e5340ce4
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-18 20:02:35 -0300

    Update PlayerMidiasMp3.jsx

--- React/youtube/src/Componentes/PlayerMidiasMp3.jsx
+++ React/youtube/src/Componentes/PlayerMidiasMp3.jsx
@@ -15,14 +15,20 @@ const PlayerMidiasMp3 = ({ effectAtualizacao, executaMidia }) => {
     const payload = {tipoMidia: 'MP3'}
 
     const [atualizacaoModiaMp3, setAtualizacaoMidiaMp3] = useState(0)
+    const [listaVazia, setListaVazia] = useState(false)
 
     useEffect(()=>{
         setAtualizacaoMidiaMp3(effectAtualizacao)        
     }, [effectAtualizacao])
 
-    const {dados, carregando, usuarioLogado} = useRequestDjango(`${urlDefaultDjango}/listagem_midias/`, payload, atualizacaoModiaMp3);
-
+    const {dados, carregando, usuarioLogado} = useRequestDjango(`${urlDefaultDjango}/listagem_midias/`, payload, atualizacaoModiaMp3);    
     if (carregando) return <img src="/img/imgBtns/loading.gif" alt="Carregando..."/>;
+
+    useEffect(() => {
+        if (dados.length > 0){
+            setListaVazia(true)
+        }
+    }, [])
     
     const executarPlayerMidia = (linkMidia) => {
         console.log('Executando mídia...')
@@ -76,39 +82,41 @@ const PlayerMidiasMp3 = ({ effectAtualizacao, executaMidia }) => {
 
     return (
         <div>
+            {listaVazia ? <>
             <h3>Lista MP3</h3>
             <div className="playerMidiasMp3-content">
-                    {dados.map((item) => (
-                        <div className="playerMidiasMp3-playMidias"  key={item.id_music}>
-                            <div className="playerMidiasMp3-div_paragraphTitulos">
-                                <p className='playerMidiasMp3-paragraphs'>{item.nome_arquivo}</p> 
-                                <p>{String.fromCodePoint(0x23F3)} {converterDuracao(item.duracao_midia)}</p>
-                            </div>
+                {dados.map((item) => (
+                    <div className="playerMidiasMp3-playMidias"  key={item.id_music}>
+                        <div className="playerMidiasMp3-div_paragraphTitulos">
+                            <p className='playerMidiasMp3-paragraphs'>{item.nome_arquivo}</p> 
+                            <p>{String.fromCodePoint(0x23F3)} {converterDuracao(item.duracao_midia)}</p>
+                        </div>
 
-                            <div>
-                                <img className="playerMidiasMp3-imgMiniatura" src={`${urlMiniatura}${item.path_miniatura}`} alt="miniatura"  />
-                            </div>
+                        <div>
+                            <img className="playerMidiasMp3-imgMiniatura" src={`${urlMiniatura}${item.path_miniatura}`} alt="miniatura"  />
+                        </div>
 
-                            <p className="playerMidiasMp3-btnsAcao">
-                                <img src="/img/imgBtns/botao-play.png" alt="player" className="playerMidiasMp3-imgBtn playerMidiasMp3-imgBtnLink" 
-                                onClick={() => executarPlayerMidia(item.path_arquivo)} />
+                        <p className="playerMidiasMp3-btnsAcao">
+                            <img src="/img/imgBtns/botao-play.png" alt="player" className="playerMidiasMp3-imgBtn playerMidiasMp3-imgBtnLink" 
+                            onClick={() => executarPlayerMidia(item.path_arquivo)} />
 
-                                <img src="/img/imgBtns/download.png" alt="download" className="playerMidiasMp3-imgBtn playerMidiasMp3-imgBtnDownload" 
-                                onClick={() => downloadMidia(item.path_arquivo)} />  
+                            <img src="/img/imgBtns/download.png" alt="download" className="playerMidiasMp3-imgBtn playerMidiasMp3-imgBtnDownload" 
+                            onClick={() => downloadMidia(item.path_arquivo)} />  
 
-                                <img src="/img/imgBtns/remover.png" alt="remover" className="playerMidiasMp3-imgBtn playerMidiasMp3-imgBtnRemover" 
-                                onClick={() => removeDeleteMidia(item.id_music)} />
+                            <img src="/img/imgBtns/remover.png" alt="remover" className="playerMidiasMp3-imgBtn playerMidiasMp3-imgBtnRemover" 
+                            onClick={() => removeDeleteMidia(item.id_music)} />
 
-                                {/*<div className="divImgLoading"><img  className="imgLoading" src="/img/imgBtns/spinner.gif" alt="Carregando..."/></div>*/}
+                            {/*<div className="divImgLoading"><img  className="imgLoading" src="/img/imgBtns/spinner.gif" alt="Carregando..."/></div>*/}
 
-                                {/** && Use quando você só quer mostrar algo se a condição for verdadeira:
-                                 *  ? Use quando você quer mostrar uma coisa OU outra, dependendo da condição:*/}
+                            {/** && Use quando você só quer mostrar algo se a condição for verdadeira:
+                             *  ? Use quando você quer mostrar uma coisa OU outra, dependendo da condição:*/}
 
-                                {/*downloadMidias == item.id_dados && (<div className="divImgLoading"><img  className="imgLoading" src="/img/imgBtns/spinner.gif" alt="Carregando..."/></div>)*/}
-                            </p>
-                        </div>                     
-                    ))}            
+                            {/*downloadMidias == item.id_dados && (<div className="divImgLoading"><img  className="imgLoading" src="/img/imgBtns/spinner.gif" alt="Carregando..."/></div>)*/}
+                        </p>
+                    </div>                     
+                ))}            
             </div>
+            </> : <h3> Não foram encontrados registro de links na base de dados. </h3>}
         </div>
     );
 };
